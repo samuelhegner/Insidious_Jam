d4c6489117287f11b248e60c0520dd0ce3bd77bd
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2018-10-30 15:13:31 +0000

    go go go

---
+++ Insidious_Project/Assets/SoulRespawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulRespawn : MonoBehaviour {
+
+     public float timeToRespawn;
+     
+     public void Collected() {
+
+
+          StartCoroutine(respawn());
+
+     }
+
+
+
+     IEnumerator respawn() {
+
+          GetComponent<Collider2D>().enabled = false;
+          transform.GetChild(0).gameObject.SetActive(false);
+          
+          yield return  new WaitForSeconds(timeToRespawn);
+
+
+          GetComponent<Collider2D>().enabled = true;
+          transform.GetChild(0).gameObject.SetActive(true);
+
+
+     }
+}
