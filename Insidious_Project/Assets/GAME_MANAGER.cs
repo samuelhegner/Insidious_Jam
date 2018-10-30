e3d16dfac4440e8ce8d75eda83dec1dc8872413d
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2018-10-30 12:55:10 +0000

    GM

---
+++ Insidious_Project/Assets/GAME_MANAGER.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GAME_MANAGER : MonoBehaviour {
+
+	public float PLayerMaxhealth;
+
+	public float CurrentHealth;
+
+	public float CurrentRage;
+
+	public float RageTarget;
+
+	public bool Raged;
+
+	public static GAME_MANAGER GM;
+	
+	void Start () {
+
+		GM = this;
+
+
+
+	}
+	
+	
+}
