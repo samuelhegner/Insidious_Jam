54ce7e7bbc5895e730dac753dac7b4b972419a3f
Samuel Hegner <C16439262@mydit.ie>
2018-10-30 12:54:58 +0000

    EnemyStart

---
+++ Insidious_Project/Assets/EnemyMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float runSpeed;
+
+    Rigidbody2D rb;
+
+    GameObject player;
+
+    public enum EnemyState{
+        patrol,
+        idle, 
+        flee
+    }
+
+    public EnemyState currentState;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        currentState = EnemyState.flee;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(currentState == EnemyState.idle){
+            rb.velocity = Vector2.zero;
+
+            if(Vector2.Distance(transform.position, player.transform.position) < 5f){
+                currentState = EnemyState.flee;
+            }
+
+        }else if(currentState == EnemyState.flee){
+            Vector2 fleedDir = (transform.position - player.transform.position);
+            fleedDir.Normalize();
+
+            rb.velocity = fleedDir * runSpeed;
+
+            if (Vector2.Distance(transform.position, player.transform.position) > 5f)
+            {
+                currentState = EnemyState.idle;
+            }
+        }
+	}
+}
---
+++ Insidious_Project/Assets/LerpCamPos.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpCamPos : MonoBehaviour {
+
+    GameObject player;
+
+    public float lerpSpeed;
+
+    Vector3 posToMove;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindWithTag("Player");
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+        posToMove = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
+
+        transform.position = Vector3.Lerp(transform.position, posToMove, Time.deltaTime);
+	}
+}
